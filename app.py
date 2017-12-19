b010138206e07489c9e14a6293f3f92980bbf471
Jae Ryoo <jryoo@turing.local>
2017-12-18 16:47:15 -0800

    name path

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
